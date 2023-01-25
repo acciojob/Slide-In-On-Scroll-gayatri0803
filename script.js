1ae0581bc07110a6bb1473df4d6e67e027b7090d
gayatri0803 <85298488+gayatri0803@users.noreply.github.com>
2023-01-25 15:04:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const sliderImages=document.querySelectorALl('.slide-in');
 function checkSlide(e) {
 	sliderImages.forEach(sliderImage=>{
 		const slideInAt=(window.scrollY+window.innerHeight)-sliderImage.height/2;
+		
 		const imageBottom=sliderImage.offsetTop+sliderImage.height;
 		const isHalfShown=slideInAt>sliderImage.offsetTop;
 		const isNotScrolledPast=window.scrollY<imageBottom;
